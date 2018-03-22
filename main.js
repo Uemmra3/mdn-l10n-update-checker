8f1b8c73d6ac04c3e3aefb87a2d9049a751508e2
Takahiro Uemura <Uemmra3@gmail.com>
2018-03-22 19:16:56 +0900

    delete var

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 
-var json;
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 console.log ('start');
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4 && xhr.status == 200) {
     //
-    json = JSON.parse(xhr.responseText);
+    const json = JSON.parse(xhr.responseText);
     //console.log(json.title);
     //console.log(json.translations[0].url);
     const org_date = new Date(json.translations[0].last_edit);
     const l10n_date = new Date(json.last_edit);
     console.log(org_date);
     console.log(l10n_date);
-    var timediff = org_date.getTime() - l10n_date.getTime();
+    const timediff = org_date.getTime() - l10n_date.getTime();
     if (timediff > 0) {
       const datediff = Math.floor(timediff/(24*3600000));
       console.log('This page is ' + datediff + ' days old!!');
       // Highlight edit UI
       //var el = document.querySelector(".page-buttons-edit");
-      var el = document.querySelector(".document-actions");
+      let el = document.querySelector(".document-actions");
       //el.setAttribute('border: red 1px;');
       el.style.backgroundColor = 'orange';
 
       // Add datediff text
       el = document.querySelector("#edit-button");
-      var insText = document.createTextNode('(' + datediff + 'days old)');
+      const insText = document.createTextNode('(' + datediff + 'days old)');
       el.parentNode.insertBefore(insText, el.nextSibling);
 
     } else {
@@ -32,7 +31,7 @@ xhr.onreadystatechange = function() {
     }
   }
 }
-var url = document.URL
+const url = document.URL
 	.replace(/#.*/, '')
 	.replace(/$.*/, '');
 console.log(url);
